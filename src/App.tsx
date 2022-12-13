5bcb68eefeb110d0128aad52089040bde39d7beb
Jean Simas <jeansimas16@gmail.com>
2022-12-13 13:50:22 -0300

    Adding React Query Provider to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { ThemeProvider } from 'styled-components'
 import Router from './Routes'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+import { QueryClientProvider, QueryClient } from 'react-query'
 
+const queryClient = new QueryClient()
 export default function App(): JSX.Element {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
       <GlobalStyle />
     </ThemeProvider>
   )
