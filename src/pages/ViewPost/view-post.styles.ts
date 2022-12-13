4a136e9e317071b57cb24a0a481dc39772285eaf
Jean Simas <jeansimas16@gmail.com>
2022-12-13 13:53:44 -0300

    Styling Post Body

--- src/pages/ViewPost/view-post.styles.ts
+++ src/pages/ViewPost/view-post.styles.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components'
 
 export const ViewPostContainer = styled.div``
+
+export const BodyPostContainer = styled.main`
+  padding: 2.4rem 2rem;
+  color: ${(props) => props.theme.colors.baseText};
+  a {
+    text-decoration: underline;
+    color: ${(props) => props.theme.colors.blue};
+  }
+  li {
+    list-style-type: none;
+    padding-left: 0.8rem;
+  }
+  h1,
+  h2,
+  h3 {
+    margin-bottom: 0.8rem;
+    border-bottom: 1px solid ${(props) => props.theme.colors.basePost};
+  }
+`
